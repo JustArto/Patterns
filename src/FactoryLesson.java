908497c0ca663bc5be61e14dc4774ef202112c72
Arto <JustArto>
2022-02-22 02:40:50 +0400

    Initial commit

---
+++ src/FactoryLesson.java
@@ -0,0 +1,36 @@
+public class FactoryLesson {
+    public static void main(String[] args) {
+        Factory factory = new Factory();
+        Car toyota = factory.create("Toyota");
+        Car audi = factory.create("Audi");
+        toyota.drive();
+        audi.drive();
+    }
+}
+
+interface Car{
+    void drive();
+}
+class Audi implements Car{
+    @Override
+    public void drive() {
+        System.out.println("Drive Audi");
+    }
+}
+
+class Toyota implements Car{
+    @Override
+    public void drive() {
+        System.out.println("Drive Toyota");
+    }
+}
+
+class Factory{
+    public Car create(String typeOfCar){
+        switch (typeOfCar){
+            case "Toyota": return new Toyota();
+            case "Audi": return new Audi();
+            default:return null;
+        }
+    }
+}
